fb394697bfe052c77ebccb4c3673cee06cef1e9f
Eugenienakouzi <94555318+Eugenienakouzi@users.noreply.github.com>
2022-10-27 15:47:45 +0200

    initialisation de mon fichier main gigii

---
+++ cmake-build-debug/CMakeFiles/Projet_C_dictionnaire.dir/gigi's_main.c.obj.d
@@ -0,0 +1,14 @@
+CMakeFiles/Projet_C_dictionnaire.dir/gigi's_main.c.obj: \
+ C:/Users/Gigi/Documents/EFREI/L2-R/informatique/C/projet/Projet_C_dictionnaire/gigi's_main.c \
+ C:/Users/Gigi/Documents/EFREI/L2-R/informatique/C/projet/Projet_C_dictionnaire/gigi's_main.h \
+ C:/PROGRA~1/JETBRA~1/CLION2~1.4/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ C:/PROGRA~1/JETBRA~1/CLION2~1.4/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/PROGRA~1/JETBRA~1/CLION2~1.4/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ C:/PROGRA~1/JETBRA~1/CLION2~1.4/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ C:/PROGRA~1/JETBRA~1/CLION2~1.4/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/PROGRA~1/JETBRA~1/CLION2~1.4/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/PROGRA~1/JETBRA~1/CLION2~1.4/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ C:/PROGRA~1/JETBRA~1/CLION2~1.4/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/PROGRA~1/JETBRA~1/CLION2~1.4/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/PROGRA~1/JETBRA~1/CLION2~1.4/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/PROGRA~1/JETBRA~1/CLION2~1.4/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h
---
+++ gigi's_main.c
@@ -0,0 +1,10 @@
+//
+// Created by Gigi on 27/10/2022.
+//
+
+#include "gigi's_main.h"
+
+void start()
+{
+    printf("Hello World");
+}
