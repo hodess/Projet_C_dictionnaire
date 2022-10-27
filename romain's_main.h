706b4ee2e4b371b23c0e804d9ec3ab4065e80abb
romain dupont <66488197+hodess@users.noreply.github.com>
2022-10-27 15:59:11 +0200

    structure du la lettre crée

--- romain's_main.h
+++ romain's_main.h
@@ -6,4 +6,14 @@
 #define PROJET_C_DICTIONNAIRE_ROMAIN_S_MAIN_H
 #include <stdio.h>
 
+//definition de la structure qui va contenir des lettres et de nombreux elements en plus
+
+typedef struct s_node_letter
+{
+    char lettre;
+    int nb_enfant;
+    liste_lecteur enfants;
+    struct s_node_letter suivant;
+}t_node_letter,*p_node_letter;
+
 #endif //PROJET_C_DICTIONNAIRE_ROMAIN_S_MAIN_H
