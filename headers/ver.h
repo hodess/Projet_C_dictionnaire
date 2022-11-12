000620fa90457a70e836111d7a1e0f2859abcf06
Morazi <morazi.mornet@gmail.com>
2022-11-12 03:43:22 +0100

    implémentation du code pour les verbes

--- headers/ver.h
+++ headers/ver.h
@@ -1,13 +1,23 @@
 //
-// Created by moraz on 08/11/2022.
+// Created by moraz on 12/11/2022.
 //
 
 #ifndef MAIN_C_VER_H
 #define MAIN_C_VER_H
-
-typedef struct node_verbe_s {
+typedef struct node_ver_s {
     char forme_flechie[40];
+    int conjugaison;        //Infinitif () = 1
+                            //Indicatif présent (IPre) = 2
+                            //Imparfait de l'indicatif (IImp) = 3
+                            //Présent du subjonctif (SPre) = 4
+    int genre;              //Mas = 1; Fem = 2; InvGen = 3
+    int pluriel;            //PL = 1; SG = 2; InvPL = 3; InvSG = 4
+    struct node_ver_s * next;
 
-}node_verbe, *p_node_verbe;
+}node_ver, *p_node_ver;
+
+p_node_ver searchVer(int conjugaison, int genre, int pluriel, p_node_ver ver);      //retourne NULL si il n'existe pas
+
+void addVer(char adding_word[40], p_node_ver spot, int conjugaison, int genre, int pluriel);
 
 #endif //MAIN_C_VER_H
