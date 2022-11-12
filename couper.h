2b02d8ae08c619506efa0f24526e62390e8fb00a
romain dupont <66488197+hodess@users.noreply.github.com>
2022-11-12 20:41:22 +0100

    ajout du travail de gigi

---
+++ couper.h
@@ -0,0 +1,10 @@
+//
+// Created by Gigi on 11/11/2022.
+//
+
+#ifndef GIGI_S_MAIN_H_COUPER_H
+#define GIGI_S_MAIN_H_COUPER_H
+#include "lecture.h"
+void sinder_petite_chaine(char*);
+
+#endif //GIGI_S_MAIN_H_COUPER_H
---
+++ gigi's_main.c
@@ -0,0 +1,13 @@
+//
+// Created by Gigi on 27/10/2022.
+//
+
+#include "gigi's_main.h"
+
+void start()
+{
+        FILE* text;
+        text = fopen("dictionnaire_non_accentue.txt","r");
+        lecture_fichier(text);
+        printf("Fini");
+}
