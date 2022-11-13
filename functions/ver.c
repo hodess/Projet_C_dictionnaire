f9a4bf3671507f6863e2c5063ed1716aa0c5361c
Morazi <morazi.mornet@gmail.com>
2022-11-13 15:08:35 +0100

    dernières corrections des fonctions add

--- functions/ver.c
+++ functions/ver.c
@@ -5,6 +5,7 @@
 #include "../headers/ver.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 p_node_ver searchVer(int conjugaison, int genre, int pluriel, p_node_ver ver){
     while (genre != ver->genre || pluriel != ver->pluriel || conjugaison != ver->conjugaison){
@@ -17,12 +18,17 @@ p_node_ver searchVer(int conjugaison, int genre, int pluriel, p_node_ver ver){
 }
 
 void addVer(char adding_word[40], p_node_ver spot, int conjugaison, int genre, int pluriel){
-    node_ver adding_node;
-    strcpy(adding_word, adding_node.forme_flechie);
-    adding_node.conjugaison = conjugaison;
-    adding_node.genre = genre;
-    adding_node.pluriel = pluriel;
-    p_node_ver temp = spot->next;
-    spot->next = &adding_node;
-    adding_node.next = temp;
+    p_node_ver adding_node = (p_node_ver) malloc(sizeof(node_ver));
+    strcpy(adding_node->forme_flechie,adding_word);
+    adding_node->conjugaison = conjugaison;
+    adding_node->genre = genre;
+    adding_node->pluriel = pluriel;
+    if (spot == NULL) {
+        spot = adding_node;
+        adding_node->next = NULL;
+    }
+    else {
+        adding_node->next = spot->next;
+        spot->next = adding_node;
+    }
 }
